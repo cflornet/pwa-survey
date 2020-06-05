c04f70421489e4ef512b909831a3e528053649c1
cflornet <andres@cflor.net>
2020-06-05 10:22:17 +0200

    modularity admin

--- index.php
+++ index.php
@@ -9,31 +9,36 @@
 	<link rel="manifest" href="manifest.json">
 	<title>Survey</title>
 	<?
-		require_once "connectDB.req.php";
+		require_once "connectDB.req.php"; // connexion db avec PDO 
+
+		session_start(); // La méthode choisie pour préserver des données entre plusieurs accès est l'utilisation des sessions https://www.php.net/manual/fr/intro.session.php
 
 		if (isset($_POST['sbm']))
 		{
 			$usr = $_POST['usr'];		
 			$pwd = $_POST['pwd'];
 
-			$sql = "SELECT COUNT(*) AS total FROM buse_user WHERE usen_status = 1 AND usec_id = '".$usr."' AND usec_pwd = '".$pwd."';";
+			$sql = "SELECT COUNT(*) AS total FROM buse_user WHERE usen_status = 1 AND usec_id = '".$usr."' AND usec_pwd = '".$pwd."';"; // Compter le nombre de lignes et Nommer un champ avec « AS » pour un login
 			$data = $pdo -> query($sql);
-			if ($data->fetchColumn() > 0)
+			if ($data->fetchColumn() > 0) // Si la première colonne depuis la première ligne du jeu de résultats est non vide, la requête SELECT à réussie 
 			{
 				$_SESSION['usr_id'] = $_POST['usr'];
+				$_SESSION['usr_status'] = 1;
 
-				$sql = "SELECT usec_name nam,usec_surname sur,usen_type typ FROM buse_user WHERE usen_status = 1 AND usec_id = '".$usr."' AND usec_pwd = '".$pwd."';";
+				$sql = "SELECT usec_name nam,usec_surname sur,usen_status sta FROM buse_user WHERE usen_status = 1 AND usec_id = '".$usr."' AND usec_pwd = '".$pwd."';"; // Les valeurs nam, sur, et typ représentent respectivement le nom, le prénom et le status de l'utilisateur. La requête teste s'il est Administrateur
 				$data2 = $pdo -> query($sql);
 				
 				while ($row = $data2 -> fetch()) 
 				{
 					
 					$_SESSION['usr_name'] = $row['nam'];
-					$_SESSION['usr_type'] = $row['typ'];
 				}
 			}
 			else
 			{
+				$_SESSION['usr_id'] = '';
+				$_SESSION['usr_name'] = '';
+				$_SESSION['usr_status'] = '0';		
 				echo "<br><br><br>
 				<div class='alert alert-danger' role='alert'>
 					<span class='sr-only'>Error:</span>
@@ -42,50 +47,45 @@
 			}
 		}
 
-		if (isset($_POST['ctu']))
+		if (isset($_POST['smd']))
 		{
-			$usr = $_POST['usu'];
-			$pwd = $_POST['pwu'];
-			$nam = $_POST['nau'];
-			$sur = $_POST['suu'];
-			$typ = $_POST['ru'];
-
-
-			$sql = "SELECT COUNT(*) AS total FROM buse_user WHERE usec_id = '".$usr."';";
-
-			$data = $pdo -> query($sql);
-			if ($data->fetchColumn() > 0)
+			$ddt = $_POST['ddt'];
+			for ($i = 6; $i <= 23; $i++)
 			{
-				echo "<br><br><br>
-				<div class='alert alert-danger' role='alert'>
-					<span class='sr-only'>Error:</span>
-					The user already exists
-				</div>";
+				$hou = $ddt." ".$_POST['hou'.$i]."00";
+				$act = $_POST['act'.$i];
+				$loc = $_POST['loc'.$i];
+				$gro = $_POST['gro'.$i];
+				$tim = $_POST['tim'.$i];
+				$des = $_POST['des'.$i];
+
+				if(strlen($des) > 5)
+				{
+					$sql3 = "INSERT INTO sdid_diary_detail(didf_hour,didf_date,didc_student,didn_category,didn_location,didn_group,didn_duration,didc_description) VALUES('".$hou."','".$ddt."','".$_SESSION['usr_id']."','".$act."',".$loc.",".$gro.",".$tim.",'".$des."');";
+					$data = $pdo -> query($sql3);
+				}
+
+				$hou = $ddt." ".$_POST['hou'.$i.'m']."00";
+				$act = $_POST['act'.$i.'m'];
+				$loc = $_POST['loc'.$i.'m'];
+				$gro = $_POST['gro'.$i.'m'];
+				$tim = $_POST['tim'.$i.'m'];
+				$des = $_POST['des'.$i.'m'];
+
+				if(strlen($des) > 5)
+				{
+					$sql3 = "INSERT INTO sdid_diary_detail(didf_hour,didf_date,didc_student,didn_category,didn_location,didn_group,didn_duration,didc_description) VALUES('".$hou."','".$ddt."','".$_SESSION['usr_id']."','".$act."',".$loc.",".$gro.",".$tim.",'".$des."');";
+					$data = $pdo -> query($sql3);
+				}
 			}
-			else
-			{
-				$sql = "INSERT INTO buse_user(usec_id,usec_pwd,usec_name,usec_surname,usen_type,usen_status) VALUES('".$usr."','".$pwd."','".$nam."','".$sur."',".$typ.",1);";
-				$data3 = $pdo -> query($sql);				
-			}
-		}
-
-		if (isset($_POST['upds']))
-		{
-			$usr = $_POST['useu'];
-			$pwd = $_POST['pwdu'];
-			$typ = $_POST['typu'];
-			$sta = $_POST['stau'];
-
-			$sql = "UPDATE buse_user SET usec_pwd = '".$pwd."',usen_type = ".$typ.",usen_status = ".$sta." WHERE usec_id = '".$usr."';";
-			//echo('<br><br><br>'.$sql);
-
-			$data = $pdo -> query($sql);
+			$sql4 = "UPDATE sdia_diary SET diac_summary='".$_POST['summ']."',dian_smiley=".$_POST['smi'].",dian_status=2 WHERE diac_student='".$_SESSION['usr_id']."' AND diaf_date='".$_POST['ddt']."';";
+			$data = $pdo -> query($sql4);
 		}
 	?>
 </head>
 <body>
 	<?
-		if($_SESSION['usr_type'] == 1)
+		if($_SESSION['usr_status'] > 0)
 		{
 			echo '<div class="container" id="cnt_1" style="display:block;">';		
 		}
@@ -104,19 +104,6 @@
 		  	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="fa="fal.$sqls;e" aria-label="Toggle navigation">
 		    	<span class="na//vbar-toggler-icon"></span>
 		  	</button>
-		  	<div class="collapse navbar-collapse" id="navbarNavDropdown">
-		    	<ul class="navbar-nav">
-		      		<li class="nav-item dropdown">
-		        		<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-		          			Actions
-		        		</a>
-		        		<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-		        			<a class="dropdown-item" href="index.php">New</a>
-		        			<a class="dropdown-item" href="balance.php">Balance</a>
-		        		</div>
-		      		</li>
-		    	</ul>
-		  	</div>
 		</nav>	
 		<div class="container">
 			<div class="row">
@@ -134,159 +121,224 @@
 					&nbsp;
 				</div>
 			</div>
-			<div class="row">
-				<div class="col align-self-end" style="text-align:right;">
-					Users
-				</div>
-			</div>
-			<hr width=100%>
 			<div class="card">
 				<div class="card-body">
 					<div class=row>
 						&nbsp;
 					</div>
-					<div class="card">
-						<div class="card-body">
-							<h6 class="card-title">New User</h6>
-							<form method="post">
-								<div class="row">
-									<div class="col">
-										<div class="form-group">
-											<label for="emu">User</label>
-											<input type="text" name="usu" placeholder="User" class="form-control"> 
-										</div>
-									</div>
-								</div>
-								<div class="row">
-									<div class="col">
-										<div class="form-group">
-											<label for="pwu">Password</label>
-											<input type="text" name="pwu" placeholder="Password" class="form-control"> 
-										</div>
-									</div>
-								</div>	
-								<div class="row">
-									<div class="col-md-6">
-										<div class="form-group">
-											<label for="nau">Name</label>
-											<input type="text" name="nau" placeholder="Name" class="form-control"> 
-										</div>
-									</div>
-									<div class="col-md-6">
-										<div class="form-group">
-											<label for="suu">Surname</label>
-											<input type="text" name="suu" placeholder="Surname" class="form-control"> 
-										</div>
-									</div>
-								</div>	
-								<div class="row">
-									<div class="col">
-										<div class="form-check form-check-inline">
-											<input class="form-check-input" type="radio" name="ru" id="ra" value="1">
-											<label class="form-check-label" for="ra">Admin</label>
-										</div>
-										<div class="form-check form-check-inline">
-											<input class="form-check-input" type="radio" name="ru" id="rn" value="2" checked="true">
-											<label class="form-check-label" for="ra">User</label>
-										</div>
-									</div>
-								</div>	
-								<div class="row">	
-									<div class="col-lg-12">
-										&nbsp;
-									</div>
-								</div>
-								<div class="row">	
-									<div class="col-lg-12">
-										<div class="form-group">
-											<input type="submit" name="ctu" class="btn btn-primary btn-lg btn-block" value="Create User">
-										</div>	
-									</div>	
-								</div>	
-							</form>
-						</div>
-					</div>
-					<div class=row>
-						&nbsp;
-					</div>
-					<div class="card">
-						<div class="card-body">
-							<h6 class="card-title">Users List</h6>
-							<div class="table-responsive">
-								<table id="tbp" class="table">
-									<thead>
-										<tr>
-											<th>User</th>
-											<th>Name</th>
-											<th>Password</th>
-											<th>Type</th>
-											<th>Status</th>
-											<th>&nbsp;</th>
-										</tr>
-									</thead>
-									<tbody>
-										<?
-											$sql2 = "SELECT usec_id usr,usec_name nam,usec_surname sur,usec_pwd pwd,usen_type typ,usen_status sta FROM buse_user;";
-											$trns = $pdo -> query($sql2);
-											while ($trn = $trns -> fetch()) 
+					<form method="post">
+						<?
+							if($_SESSION['usr_status'] > 0)
+							{										
+								$sql2 = "SELECT diaf_date dat, diac_teacher tea  FROM sdia_diary WHERE dian_status = 1 AND diac_student = '".$_SESSION['usr_id']."';";
+								$trns = $pdo -> query($sql2);
+								
+								while ($trn = $trns -> fetch()) 
+								{
+									echo 	'
+												<div class="row">
+													<div class="col align-self-end" style="text-align:right;">
+														Teacher:
+											'
+											.$trn['tea'].
+											'
+													</div>
+												</div>
+												<hr width="100%">
+												<div class="card">
+													<div class="card-body">
+														<h6 class="card-title">
+											'
+											.$trn['dat'].
+											'
+														</h6>
+											<input type="hidden" name="ddt" value="'.$trn['dat'].'">';
+	
+											for ($i = 6; $i <= 23; $i++)
 											{
-												if ($trn['typ'] == 1)
-												{
-													$typv = "<select class='form-control' name='typu'>
-  																<option value='1' selected>Admin</option>
-  																<option value='2'>User</option>
-  															</select>";
-												}
-												else
-												{
-													$typv = "<select class='form-control' name='typu'>
-  																<option value='1'>Admin</option>
-  																<option value='2' selected>User</option>
-  															</select>";
-												}
-												if ($trn['sta'] == 1)
-												{
-													$stav = "<select class='form-control' name='stau'>
-  																<option value='1' selected>Active</option>
-  																<option value='2'>Retired</option>
-  															</select>";
-												}
-												else
-												{
-													$stav = "<select class='form-control' name='stau'>
-  																<option value='1'>Active</option>
-  																<option value='2' selected>Retired</option>
-  															</select>";
-												}
-  												echo "<form method='post'>
-  														<input type='hidden' name='useu' value='".$trn['usr']."'>
-  													  	<tr>
-  															<td>".$trn['usr']."</td>
-  															<td>".$trn['nam']." ".$trn['sur']."</td>
-  															<td>
-  																<input class='form-control' type='text' name='pwdu' value='".$trn['pwd']."'>
-  															</td>
-  															<td>".$typv."</td>
-  															<td>".$stav."</td>
-  															<td>
-  																<input type='submit' class='btn btn-primary' name='upds' value='Update'>
-  															</td>
-  													  	</tr>
-  													  </form>";
-  											}
-										?>
-									</tbody>
-								</table>	
-							</div>
-						</div>	
-					</div>
+												echo 	'
+														<div class="row">
+															<div class="col-md-3">
+																<div class="form-group">
+																	<label for="hou'.$i.'">Hour</label>
+																	<input type="text" class="form-control" name="hou'.$i.'" value="'.$i.':00" readonly>
+																</div>
+															</div>
+															<div class="col-md-2">
+																<div class="form-group">
+																	<label for="act'.$i.'">Activity</label>
+																	<select name="act'.$i.'" class="form-control">
+																		<option value="1">Reading</option>
+																		<option value="2">Writing</option>
+																	</select>
+																</div>
+															</div>
+															<div class="col-md-3">
+																<div class="form-group">
+																	<label for="loc'.$i.'">Location</label>
+																	<select name="loc'.$i.'" class="form-control">
+																		<option value="1">Home</option>
+																		<option value="2">University</option>
+																		<option value="3">Library</option>
+																	</select>
+																</div>
+															</div>
+															<div class="col-md-2">
+																<div class="form-group">
+																	<label for="gro'.$i.'">Group</label>
+																	<select name="gro'.$i.'" class="form-control">
+																		<option value="1">Self</option>
+																		<option value="2">Group Learning</option>
+																	</select>
+																</div>
+															</div>
+															<div class="col-md-2">
+																<div class="form-group">
+																	<label for="tim'.$i.'">Time</label>
+																	<input type="number" class="form-control" name="tim'.$i.'" value="0" min="0" max="24" step="0.5">
+																</div>
+															</div>
+														</div>
+														<div class="row">
+															<div class="col">
+																<div class="form-group">
+																	<label for="des'.$i.'">Description</label>
+																	<textarea name="des'.$i.'" class="form-control"></textarea>
+																</div>
+															</div>
+														</div>
+														<hr width="100%">
+
+														<div class="row">
+															<div class="col-md-3">
+																<div class="form-group">
+																	<label for="hou'.$i.'m">Hour</label>
+																	<input type="text" class="form-control" name="hou'.$i.'m" value="'.$i.':30" readonly>
+																</div>
+															</div>
+															<div class="col-md-2">
+																<div class="form-group">
+																	<label for="act'.$i.'m">Activity</label>
+																	<select name="act'.$i.'m" class="form-control">
+																		<option value="1">Reading</option>
+																		<option value="2">Writing</option>
+																	</select>
+																</div>
+															</div>
+															<div class="col-md-3">
+																<div class="form-group">
+																	<label for="loc'.$i.'m">Location</label>
+																	<select name="loc'.$i.'m" class="form-control">
+																		<option value="1">Home</option>
+																		<option value="2">University</option>
+																		<option value="3">Library</option>
+																	</select>
+																</div>
+															</div>
+															<div class="col-md-2">
+																<div class="form-group">
+																	<label for="gro'.$i.'m">Group</label>
+																	<select name="gro'.$i.'m" class="form-control">
+																		<option value="1">Self</option>
+																		<option value="2">Group Learning</option>
+																	</select>
+																</div>
+															</div>
+															<div class="col-md-2">
+																<div class="form-group">
+																	<label for="tim'.$i.'m">Time</label>
+																	<input type="number" class="form-control" name="tim'.$i.'m" value="0" min="0" max="24" step="0.5">
+																</div>
+															</div>
+														</div>
+														<div class="row">
+															<div class="col">
+																<div class="form-group">
+																	<label for="des'.$i.'m">Description</label>
+																	<textarea name="des'.$i.'m" class="form-control"></textarea>
+																</div>
+															</div>
+														</div>
+														<hr width="100%">
+														';
+											}
+
+											echo 
+											'	
+														<div class="row">
+															<div class="col">
+																<div class="form-group">
+																	<label for="summ">Summary</label>
+																	<textarea name="summ" class="form-control"></textarea>
+																</div>
+															</div>
+														</div>	
+														<div class="row">
+															<div class="col">
+																<div class="form-group">
+																	<label>Please thick the box</label>
+																</div>
+															</div>
+														</div>
+														<div class="row">
+															<div class="col">
+																<div class="form-check form-check-inline">
+																	<input class="form-check-input" type="radio" name="smi" id="smi1" value="1" checked>
+																	<i class="far fa-smile" style="font-size:24px"></i>
+																	&nbsp;
+																	<label class="form-check-label" for="smi1">Happy</label>
+																</div>																
+																<div class="form-check form-check-inline">
+																	<input class="form-check-input" type="radio" name="smi" id="smi2" value="2">
+																	<i class="far fa-tired" style="font-size:24px"></i>
+																	&nbsp;
+																	<label class="form-check-label" for="smi2">Tired</label>
+																</div>																
+																<div class="form-check form-check-inline">
+																	<input class="form-check-input" type="radio" name="smi" id="smi3" value="3">
+																	<i class="far fa-sad-tear" style="font-size:24px"></i>
+																	&nbsp;
+																	<label class="form-check-label" for="smi3">Sad</label>
+																</div>																
+																<div class="form-check form-check-inline">
+																	<input class="form-check-input" type="radio" name="smi" id="smi4" value="4">
+																	<i class="far fa-meh-rolling-eyes" style="font-size:24px"></i>
+																	&nbsp;
+																	<label class="form-check-label" for="smi4">Worried</label>
+																</div>																
+																<div class="form-check form-check-inline">
+																	<input class="form-check-input" type="radio" name="smi" id="smi5" value="5">
+																	<i class="far fa-dizzy" style="font-size:24px"></i>
+																	&nbsp;
+																	<label class="form-check-label" for="smi5">Sick</label>
+																</div>																
+															</div>
+														</div>	
+													</div>
+												</div>
+												<hr width=100%>
+												<div class="row">	
+													<div class="col-lg-12">
+														<div class="form-group" align="right">
+															<input type="submit" name="smd" class="btn btn-primary btn-lg btn-block" value="Send">
+														</div>	
+													</div>	
+												</div>	
+											';
+								}
+							}
+						?>
+					</form>
 				</div>
+			</div>
+			<div class=row>
+				&nbsp;
 			</div>
 		</div>
 	</div>
 
 	<?
-		if($_SESSION['usr_type'] == 1)
+		if($_SESSION['usr_status'] > 0)
 		{
 			echo '<div class="container" id="cnt_2" style="display:none;">';		
 		}
@@ -344,6 +396,7 @@
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/numeral.js/2.0.6/numeral.min.js"></script>
+    <script src='https://kit.fontawesome.com/a076d05399.js'></script>
 	<script src="sw.js"></script>
   	<script>
     	if ('serviceWorker' in navigator)
